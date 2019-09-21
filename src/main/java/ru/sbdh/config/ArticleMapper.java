4fcd956204c0f8ef4b78002ecfad75892c90179c
Your Name <you@example.com>
2019-09-21 08:57:01 +0400

    revert

--- src/main/java/ru/sbdh/config/ArticleMapper.java
+++
@@ -1,9 +0,0 @@
-package ru.sbdh.config;
-
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
-public interface ArticleMapper {
-    @Select("SELECT * FROM ARTICLES WHERE id = #{id}")
-    Article getArticle(@Param("id") Long id);
-}
